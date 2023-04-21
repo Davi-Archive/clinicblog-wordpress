ef83db3d30e51f33b8e7ee63616547c186b94bfd
d1av <davi4alves@gmail.com>
2023-04-21 16:10:07 -0300

    feat: :sparkles: add components

---
+++ wp-content/themes/drdavi/header.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html class="no-js" lang="ptBR">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title><?php the_title() ?></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo get_template_directory_uri() ?>/img/favicon.png">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style.css">
+    <!-- Inicio Wordpress Head-->
+    <?php wp_head(); ?>
+    <!-- Fim Wordpress Head-->
+</head>
+
+<body>
+    <div class="smart-page-loader" style="display: none;">
+        <div class="wp-smart-loader smart-loader-two">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+
+    <header class="top-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="head-right">
+                        <div class="head-img1">
+                            <img src="<?php echo get_template_directory_uri() ?>/img/c-1.png" alt="">
+                        </div>
+                        <span>4500 Davi Mercntile plaza,Suite 300, Fort Worth,TX,76137,USA</span>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="head-left">
+                        <div class="head-img">
+                            <img src="<?php echo get_template_directory_uri() ?>/img/c-2.png" alt="">
+                        </div>
+                        <span>+1(817) 901 3377, +1(817) *** 3377</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <header class="header-section">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="https://products.webrockmedia.com/dentallab-html/index.html"><img src="<?php echo get_template_directory_uri() ?>/img/logo.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="modal" data-target="#myModal1" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="https://products.webrockmedia.com/dentallab-html/index.html"><span>Home</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="https://products.webrockmedia.com/dentallab-html/aboutus.html">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="https://products.webrockmedia.com/dentallab-html/services.html">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="https://products.webrockmedia.com/dentallab-html/departments.html">Departments</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="https://products.webrockmedia.com/dentallab-html/blog.html">Blogs</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="https://products.webrockmedia.com/dentallab-html/contactus.html">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
